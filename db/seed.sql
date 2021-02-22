b7c8a999cefbe7cce72ef7f34febe8921706ba5d
CBanks <banksc7669@yahoo.com>
2021-02-22 02:19:36 -0500

    package json and express handlebars installed

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Mustard',False);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Peppers',False);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Pickles',False);
